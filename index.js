119e528b17e4946820a22fb11e8e2aa747eb8a3f
Samwa-ua <39631461+Samwa-ua@users.noreply.github.com>
2022-02-14 21:35:58 +0200

    added files manually

---
+++ index.js
@@ -0,0 +1,26 @@
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegree = (seconds / 60) * 360 + 90;
+
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegree = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegree = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+
+  document.querySelector(".seconds").innerHTML = seconds;
+  document.querySelector(".minutes").innerHTML = minutes;
+  document.querySelector(".hours").innerHTML = hours;
+}
+
+setInterval(setDate, 1000);
